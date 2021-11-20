007eaadbb674973c5999e57ff3892589ec1c195b
Maxime Dupont <m-dupont01@outlook.fr>
2021-11-20 21:35:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Chargement des modules 
+var express = require('express');
+var app = express();
+
+// cf. https://www.npmjs.com/package/socket.io#in-conjunction-with-express
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+server.listen(8080, function() {
+    console.log("C'est parti ! En attente de connexion sur le port 8080...");
+});
+
+// Configuration d'express pour utiliser le répertoire "public"
+app.use(express.static('public'));
+// set up to 
+app.get('/', function(req, res) {  
+    res.sendFile(__dirname + '/public/theGame.html');
+});
