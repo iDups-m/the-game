76b9a8738267719472333071987384458465930b
pierre <pierre.odrion@orange.fr>
2021-11-22 19:21:04 +0100

    commit the display of the cards (html/css)

--- public/theGame.js
+++ public/theGame.js
@@ -2,24 +2,20 @@
  ***                  useful functions of the game                  ***
  **********************************************************************/
 
-function createCard(faceClass, specClass) {
-    let span = document.createElement("SPAN");
-    span.classList.add("card");
-    let div = document.createElement("DIV");
-    div.classList.add(faceClass, specClass);
-    span.appendChild(div);
-    return span;
+function shuffle(deck) {
+
+}
+
+function getCards(hand) {
+    for(let i = 0; i < hand.length; i++) {
+        //find the current card
+    }
+}
+
+function deal(deck) {
+    shuffle(deck);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     let sock = io.connect();
-
-    document.getElementById("base")
-
-    /*document.getElementById("board").addEventListener("click", function (e) {
-        let card = e.target.parentNode;
-        if (e.target.tagName === "DIV" && card.tagName === "SPAN") {
-            card.classList.toggle("flip");
-        }
-    });*/
 });
